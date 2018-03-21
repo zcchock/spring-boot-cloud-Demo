6b362a249f446d3ef7b292d7af4ac5d5ddc1a701
zcchock <zc26616@126.com>
2018-03-21 14:38:10 +0800

    网关对接口访问的过滤

---
+++ zuul-gateway-filter/src/main/java/com/zcchock/filter/PreRequestLogFilter.java
@@ -0,0 +1,41 @@
+
+package com.zcchock.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreRequestLogFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(PreRequestLogFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        PreRequestLogFilter.logger.info(String.format("send %s request to %s",
+                request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
