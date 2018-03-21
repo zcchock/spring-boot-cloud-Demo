db29acca11b365a45b2aaee277dc2d6e91ae7a4a
zcchock <zc26616@126.com>
2018-03-21 13:48:42 +0800

    ribbon属性自定义模块

---
+++ consumer-movie-ribbon-properties/src/main/java/com/zcchock/consumermovie/ConsumerMovieRibbonPropertiesApplication.java
@@ -0,0 +1,24 @@
+
+package com.zcchock.consumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConsumerMovieRibbonPropertiesApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMovieRibbonPropertiesApplication.class, args);
+	}
+}
