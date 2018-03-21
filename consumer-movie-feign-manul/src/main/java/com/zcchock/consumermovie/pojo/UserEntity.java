c3e95ab9b70f34c8434b965ed849a1bd20b3c0af
zcchock <zc26616@126.com>
2018-03-21 14:25:16 +0800

    feign模块：手动创建feign

---
+++ consumer-movie-feign-manul/src/main/java/com/zcchock/consumermovie/pojo/UserEntity.java
@@ -0,0 +1,44 @@
+
+package com.zcchock.consumermovie.pojo;
+
+import java.util.Date;
+
+public class UserEntity {
+
+    private String id;
+    private String sex;
+    private Long tall;
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Long getTall() {
+        return tall;
+    }
+
+    public void setTall(Long tall) {
+        this.tall = tall;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
