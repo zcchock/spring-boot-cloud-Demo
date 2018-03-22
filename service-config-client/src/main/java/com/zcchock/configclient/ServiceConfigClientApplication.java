198fce376615f0108daf187615aa28a95e1f288d
zcchock <zc26616@126.com>
2018-03-22 14:25:59 +0800

    config server client 统一微服务管理

---
+++ service-config-client/src/main/java/com/zcchock/configclient/ServiceConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.zcchock.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigClientApplication.class, args);
+	}
+}
